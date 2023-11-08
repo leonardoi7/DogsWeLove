14879b87cff7bd6d9de3e7b8eb01d40d65f27e83
Leonardo Quintero <6802237+leonardoi7@users.noreply.github.com>
2023-11-08 11:44:52 -0800

    Initial Commit

---
+++ DogWeLove/DogWeLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogWeLoveApp.swift
+//  DogWeLove
+//
+//  Created by leonardo on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DogWeLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
