0e3275d5f89edf482108d18869bdaf5ea3faaafb
Leonardo Quintero <6802237+leonardoi7@users.noreply.github.com>
2023-11-09 03:53:09 -0800

    Update DogData.swift

--- DogWeLove/Model/DogData.swift
+++ DogWeLove/Model/DogData.swift
@@ -7,6 +7,22 @@ struct DogData: Codable, Identifiable {
     var age: Int
     var image: String
     
+    enum CodingKeys: String, CodingKey {
+        case dogName
+        case description
+        case age
+        case image
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        dogName = try container.decode(String.self, forKey: .dogName)
+        description = try container.decode(String.self, forKey: .description)
+        age = try container.decode(Int.self, forKey: .age)
+        image = try container.decode(String.self, forKey: .image)
+        id = UUID()
+    }
+    
     init(id: UUID = UUID(), dogName: String, description: String, age: Int, image: String) {
         self.id = id
         self.dogName = dogName
