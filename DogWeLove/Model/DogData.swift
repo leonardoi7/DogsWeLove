964c5cb2f919fc85849b4f1c8aa1ea3e86b3aafb
Leonardo Quintero <6802237+leonardoi7@users.noreply.github.com>
2023-11-09 03:01:19 -0800

    Update DogData.swift

--- DogWeLove/Model/DogData.swift
+++ DogWeLove/Model/DogData.swift
@@ -1,8 +1,17 @@
 import Foundation
 
-struct DogData: Codable {
+struct DogData: Codable, Identifiable {
+    let id: UUID
     var dogName: String
     var description: String
     var age: Int
     var image: String
+    
+    init(id: UUID = UUID(), dogName: String, description: String, age: Int, image: String) {
+        self.id = id
+        self.dogName = dogName
+        self.description = description
+        self.age = age
+        self.image = image
+    }
 }
