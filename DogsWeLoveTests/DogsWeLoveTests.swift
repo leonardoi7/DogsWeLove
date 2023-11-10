fb36349441874092687733d82f0e34835eaa98d7
Leonardo Quintero <6802237+leonardoi7@users.noreply.github.com>
2023-11-10 06:27:36 -0800

    Update DogsWeLoveTests.swift
    added testFetchDogs

--- DogsWeLoveTests/DogsWeLoveTests.swift
+++ DogsWeLoveTests/DogsWeLoveTests.swift
@@ -3,27 +3,21 @@ import XCTest
 
 final class DogsWeLoveTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testFetchDogs() {
+        let expectation = XCTestExpectation(description: "fetchDogs completes")
+        var responseError: Error?
+        var responseDogs: [DogData]?
+        
+        APIManager.shared.fetchDogs { dogs, error in
+            responseDogs = dogs
+            responseError = error
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 10.0)
+        
+        XCTAssertNil(responseError, "fetchDogs should not return an error")
+        XCTAssertNotNil(responseDogs, "fetchDogs should return a list of dogs")
+        XCTAssertEqual(responseDogs?.count, 5, "fetchDogs should return 5 dogs")
     }
-
 }
